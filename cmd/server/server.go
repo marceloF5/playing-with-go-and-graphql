a21b8b1752c2690275e05fc9d09edac5478734b5
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2023-03-28 21:21:47 -0300

    feat: Setup graphql

---
+++ cmd/server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/marcelof5/playing-with-go-and-graphql/internal/database"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/marcelof5/playing-with-go-and-graphql/graph"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
